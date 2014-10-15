539ee67d07c334046c296b0182adca3b9c2cd0c1
Dslab User <dslab@linuxlabmanual.dslab.utdallas.edu>
2014-10-15 12:46:52 -0500

    jprobe do_sys_open module

--- FileWatch/filewatch.c
+++ FileWatch/filewatch.c
@@ -1,33 +1,42 @@
-/* Make sure linux-headers are installed if not,
- * `sudo apt-get install linux-headers-$(uname -r)`
- * should do the trick on debian based distros
- */
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/kprobes.h>
 
-#include <linux/module.h>       /* Needed by all modules */
-#include <linux/kernel.h>       /* Needed for KERN_INFO */
-#include <linux/init.h>         /* Needed for the macros */
-#include <linux/inotify.h>
-#include <linux/vmalloc.h>
+
+long jdo_sys_open(int dfd, const char __user *filename, int flags, umode_t mode)
+{
+	printk("do_sys_open: %s\n",filename);
+	jprobe_return();
+	return 0;
+}
+
+static struct jprobe my_jprobe = {
+	.kp = {
+		.symbol_name = "do_sys_open",
+	},
+	.entry = (kprobe_opcode_t *) jdo_sys_open,
+};
+
+static int __init my_init(void)
+{
+	int ret;
+
+	if ((ret = register_jprobe(&my_jprobe)) <0) {
+		printk("register_jprobe failed, returned %d\n", ret);
+		return -1;
+	}
+	printk("Planted jprobe at %p, handler addr %p\n",
+	       my_jprobe.kp.addr, my_jprobe.entry);
+	return 0;
+}
+
+static void __exit my_exit(void)
+{
+	unregister_jprobe(&my_jprobe);
+	printk("jprobe unregistered\n");
+}
+
+module_init(my_init);
+module_exit(my_exit);
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Swair Shah");
-MODULE_DESCRIPTION("Module to Monitor File Events");
-
-static struct inotify_event *event;
-static struct inotify_handle *handle;
-
-static void watch_file(void) {
-}
-
-static int __init start_hook(void) {  
-    printk(KERN_INFO "Loading filewatch module...\n");
-    watch_file();
-    return 0;
-}
-
-static void __exit end_hook(void) {
-    printk(KERN_INFO "Unloading the filewatch Module.\n");
-}
-
-module_init(start_hook);
-module_exit(end_hook);
