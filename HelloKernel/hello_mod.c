6a7e4f8a78eadb9140b54feb0971f92804df0d01
Swair Shah <swairshah@gmail.com>
2014-10-15 14:30:25 -0400

    minor changes hello_mod

--- HelloKernel/hello_mod.c
+++ HelloKernel/hello_mod.c
@@ -6,21 +6,20 @@
 #include <linux/module.h>       /* Needed by all modules */
 #include <linux/kernel.h>       /* Needed for KERN_INFO */
 #include <linux/init.h>         /* Needed for the macros */
-#include <linux/inotify.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Swair Shah");
 MODULE_DESCRIPTION("a Hello-Kernel Module");
 
-static int __init start_hook(void) {  
+static int __init init_function(void) {  
     printk(KERN_INFO "Loading hello-kernel module...\n");
     printk(KERN_INFO "Hello Kernel!\n");
     return 0;
 }
 
-static void __exit end_hook(void) {
+static void __exit end_function(void) {
     printk(KERN_INFO "Unloading the hello-kernel Module.\n");
 }
 
-module_init(start_hook);
-module_exit(end_hook);
+module_init(init_function);
+module_exit(end_function);
