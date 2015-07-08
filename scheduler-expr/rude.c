e3c58dc85fa574cb6050a09c6b7978bd1aab697d
Swair Shah <swairshah@gmail.com>
2015-07-08 13:45:18 -0500

    update rude.c

--- scheduler-expr/rude.c
+++ scheduler-expr/rude.c
@@ -3,11 +3,26 @@
 #include <sys/resource.h>
 #include <unistd.h>
 
+void primes(int n) {
+    int i = 3, count, c;
+    for ( count = 2 ; count <= n ; ) {
+        for ( c = 2 ; c <= i - 1 ; c++ ) {
+            if ( i%c == 0 )
+            break;
+        }
+        if ( c == i ) {
+            //printf("%d\n",i);
+            count++;
+        }
+        i++;
+    } 
+}
+
 int main() {
     int i;
     int my_pid = getpid();
     nice(-20);
-    for (i = 0; i < 1000 ; i++) {
+    for (i = 0; i < 100 ; i++) {
         int j = 10000;
         while(j > 0) {
             int k = 10000;
@@ -16,5 +31,7 @@ int main() {
             }
             j--;
         }
+        primes(i);
     }
+    printf("end!");
 }
